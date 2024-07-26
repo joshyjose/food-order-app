a8e73802c59e3866f4aa600b9deedf3e6a648567
Joshy-Jose <josejoshy12@gmail.com>
2024-07-26 14:11:39 +0200

    Add form data to database

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -2,6 +2,8 @@ import sql from "better-sqlite3";
 import { resolve } from "styled-jsx/css";
 import slugify from "slugify";
 import xss from "xss";
+import fs from "node:fs";
+import { error } from "node:console";
 
 const db = sql("meals.db");
 
@@ -17,4 +19,33 @@ export function getMeal(slug) {
 export async function saveMeal(meal) {
   meal.slug = slugify(meal.title, { lower: true });
   meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed");
+    }
+  });
+  meal.image = `/image/${fileName}`;
+
+  db.prepare(
+    `
+  INSERT INTO meals
+    (title, summary, instructions, creator, creator_email, image, slug)
+  VALUES (
+    @title,
+    @summary,
+    @instructions,
+    @creator,
+    @creator_email,
+    @image,
+    @slug
+  )
+`
+  ).run(meal);
 }
